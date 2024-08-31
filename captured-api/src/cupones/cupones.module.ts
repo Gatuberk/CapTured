c7af429d96c0c2b5aa8a0e429cfc0e497d7b429f
DilanBernal <dilanbernal01@gmail.com>
2024-08-31 13:01:30 -0500

    API con User
    Se incluyo la mayor parte del api con user, haciendo las busquedas respectivas de cada uno

---
+++ captured-api/src/cupones/cupones.module.ts
@@ -0,0 +1,14 @@
+import { CuponesService } from './cupones.service';
+/*
+https://docs.nestjs.com/modules
+*/
+
+import { Module } from '@nestjs/common';
+
+@Module({
+    imports: [],
+    controllers: [],
+    providers: [
+        CuponesService,],
+})
+export class CuponesModule { }
---
+++ captured-api/src/interfaces/users.interface.ts
@@ -0,0 +1,9 @@
+export interface userDto{
+    nombre:string,
+    apellido:string,
+    username:string,
+    direccion:string,
+    identificacion:string,
+    telefono:string,
+    password:string,    
+}
